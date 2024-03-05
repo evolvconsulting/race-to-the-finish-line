5ef7ef667df19839a7bc121e25a75e5a238d25de
Chris Schneider <chris.schneider@evolvconsulting.com>
2024-03-05 10:28:40 -0600

    add column selector

--- setup/streamlit/Home.py
+++ setup/streamlit/Home.py
@@ -7,7 +7,13 @@ common.display_header(pageHeader='Home')
 
 #setup some constant values for Colums to display
 #initialize session state variables
-st.session_state['display_columns'] = 6
+#default to 4
+if 'display_columns' not in st.session_state:
+    st.session_state['display_columns'] = 4
+
+cols = st.selectbox('How many columns do you want to display?',index=(st.session_state['display_columns']-1),options=(1,2,3,4,5,6,7,8))
+if cols is not None:
+    st.session_state['display_columns'] = cols 
 
 st.write('Imagine that each time a user came up with a new measurement to report on, they had to wait days or even weeks for an SDLC to run the requirement through its paces before being able to see the metric in their dashboard.  This project introduces a simple but effective solution improving speed to market with your application by introducing a data-driven approach and framework to adding metrics to your dashboard.')
 
